ec6e0faaf34f8d4766afb3571b87ac0d84d64217
enmanuelbayen <enmanuel0495@gmail.com>
2023-06-13 16:19:09 -0400

    Add dispatch for remove book

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import Book from './Book';
 import { useSelector } from 'react-redux';
+import Book from './Book';
 
 const BookList = () => {
-  const { books } = useSelector((state) => {state.books})
- 
- return (
-  <div>
-    {books.map((book) =>{
-      <Book
-        id={book.item_id}
-        title={book.title}
-        author={book.author}
-      />
-    })}
-  </div>
- )
+  const { books } = useSelector((state) => state.books);
+
+  return (
+    <div>
+      {books.map((book) => (
+        <Book
+          key={book.item_id}
+          id={book.item_id}
+          title={book.title}
+          author={book.author}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default BookList;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,51 +1,53 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/booksSlice';
 
-const Form = () => { 
-const [title, setTitle] = useState('');
-const [author, setAuthor] = useState('');
-const dispatch = useDispatch();
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
 
-const handleAddBook = (e) => {
-  e.preventDefault();
-  const newBook = {
-    item_id: uuidv4(), 
-    title,
-    author,
-};
+  const handleAddBook = (e) => {
+    e.preventDefault();
+    const newBook = {
+      item_id: uuidv4(),
+      title,
+      author,
+    };
 
-dispatch(addBook(newBook));
-setTitle('');
-setAuthor('');
-};
+    dispatch(addBook(newBook));
+    setTitle('');
+    setAuthor('');
+  };
 
- return (
-  <div>
-    <h3 className="book-title">Add new book</h3>
-    <form>
-      <input 
-      type="text" 
-      placeholder="Book Title" 
-      value={title} 
-      onChange={(e) => setTitle(e.target.value)}
-      />
-      <input 
-      type="text" 
-      placeholder="Author"
-      value={author} 
-      onChange={(e) => setAuthor(e.target.value)}
-      />
-      <button 
-      type="button" 
-      className="button" 
-      id="addBttn"
-      onClick={handleAddBook}
-      >ADD BOOK</button>
-    </form>
-  </div>
-)
+  return (
+    <div>
+      <h3 className="book-title">Add new book</h3>
+      <form>
+        <input
+          type="text"
+          placeholder="Book Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <button
+          type="button"
+          className="button"
+          id="addBttn"
+          onClick={handleAddBook}
+        >
+          ADD BOOK
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Form;
